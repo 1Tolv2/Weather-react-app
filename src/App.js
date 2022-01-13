7a9c127556e0754739bb558966fee6e228615aca
Sofia <sofiaq1@gmail.com>
2022-01-13 23:10:59 +0100

    Started adding geolocation

--- src/App.js
+++ src/App.js
@@ -12,7 +12,23 @@ const StyledTitle = styled.h1`
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [today, setToday] = useState(null);
+  const [coordinates, setCoordinates] = useState({});
+
+  function showPosition(position) {
+    setCoordinates({
+      lat: position.coords.latitude,
+      lon: position.coords.longitude,
+    });
+  }
+  function getLocation() {
+    return navigator.geolocation
+      ? navigator.geolocation.getCurrentPosition(showPosition)
+      : "Geolocation is not supported by this browser";
+  }
+
   useEffect(() => {
+    getLocation();
+    console.log(coordinates);
     const location = { lat: 59.3293, lon: 18.0686 };
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&exclude=alert&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
 
@@ -34,7 +50,6 @@ function App() {
     const day = new Date();
     setToday(dayList[day.getDay() - 1]);
   }, []);
-  console.log(weatherData);
 
   return (
     <>
