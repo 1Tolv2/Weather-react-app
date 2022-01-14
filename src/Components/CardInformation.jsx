bb3a9d3468507ff089e4e63417845ac65c595f20
Sofia <sofiaq1@gmail.com>
2022-01-14 10:11:10 +0100

    styled CardInformation

--- src/Components/CardInformation.jsx
+++ src/Components/CardInformation.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const StyledContainer = styled.div`
+padding: 10px;
+`
 export default function CardInformation(props) {
     return (
-        <div>
+        <StyledContainer>
             {props.children}
-        </div>
+        </StyledContainer>
     )
 }
