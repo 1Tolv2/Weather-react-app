503c104c1e1daa4cf6c6b366c5c05e2a21490046
Sofia <sofiaq1@gmail.com>
2022-01-14 14:21:27 +0100

    locationData fixed and working

--- src/App.js
+++ src/App.js
@@ -12,25 +12,13 @@ const StyledTitle = styled.h1`
 function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [today, setToday] = useState(null);
-  const [coordinates, setCoordinates] = useState({});
 
-  function showPosition(position) {
-    setCoordinates({
+  function getData(position) {
+    const coordinates = {
       lat: position.coords.latitude,
       lon: position.coords.longitude,
-    });
-  }
-  function getLocation() {
-    return navigator.geolocation
-      ? navigator.geolocation.getCurrentPosition(showPosition)
-      : "Geolocation is not supported by this browser";
-  }
-
-  useEffect(() => {
-    getLocation();
-    console.log(coordinates);
-    const location = { lat: 59.3293, lon: 18.0686 };
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${location.lat}&lon=${location.lon}&exclude=alert&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
+    };
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&exclude=alert&appid=${process.env.REACT_APP_API_KEY}&units=metric`;
 
     fetch(url)
       .then((res) => res.json())
@@ -49,6 +37,10 @@ function App() {
     ];
     const day = new Date();
     setToday(dayList[day.getDay() - 1]);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => getData(position));
   }, []);
 
   return (
