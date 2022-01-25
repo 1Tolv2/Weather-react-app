726ceb1a84e7d9c5b35851b72806447b3e3c17e0
Sofia <sofiaq1@gmail.com>
2022-01-26 00:42:46 +0100

    Added sunrise and sunset variations of the color

--- src/Components/WeatherBackground.jsx
+++ src/Components/WeatherBackground.jsx
@@ -1,34 +1,38 @@
-import React, { useContext } from "react";
-import { weatherContext } from "../App";
-
+import React from "react";
 
 const weatherBackground = {
-    day: { good: "#adcbff, #5c87ff", bad: "#cdd6e5, #7d93cf" },
-    night: { good: "#002f80, #00144d", bad: "#212d40, #16203c" },
-  };
+    sunrise: { good: "#6d9bc3, #EEC557, #FF8552", bad: "#6d9bc3, #EEC557, #FF8552" },
+  day: { good: "#adcbff, #5c87ff", bad: "#cdd6e5, #7d93cf" },
+  sunset: { good: "#754694, #EEC557, #FF8552", bad: "#754694, #EEC557, #FF8552" },
+  night: { good: "#002f80, #00144d", bad: "#212d40, #16203c" },
+};
 
-  function dayOrNight(sunrise, sunset, currentTime) {
-    console.log(currentTime)
-    return currentTime >= sunrise && currentTime <= sunset
-      ? "day"
-      : currentTime < sunrise
-      ? "night"
-      : currentTime > sunset
-      ? "night"
-      : "day";
-  }
+function dayOrNight(sunrise, sunset, currentTime) {
+  const hourOfSunrise = new Date(sunrise * 1000).getHours();
+  const hourOfSunset = new Date(sunset * 1000).getHours();
+  const hourOfDay = new Date(sunset * 1000).getHours();
 
-  function goodOrBadWeather(weatherCode) {
-    const goodWeatherCodes = [500, 501, 600, 601, 701, 800, 801, 802];
-    return goodWeatherCodes.includes(weatherCode) ? "good" : "bad";
+  if (hourOfDay === hourOfSunrise) {
+    return "sunrise";
+  } else if (hourOfDay > hourOfSunrise && hourOfDay < hourOfSunset) {
+    return "day";
+  } else if (hourOfDay === hourOfSunset) {
+    return "sunset"
+  } else if (hourOfDay > hourOfSunset || hourOfDay < hourOfSunrise) {
+      return "night"
   }
-  
-export default function WeatherBackground(props) {
-  const { weatherData } = useContext(weatherContext);
-console.log(props)
-    return (
-        `${weatherBackground[
-        dayOrNight(props.sunrise, props.sunset, props.dt)
-      ][goodOrBadWeather(props.weather[0].id)]}`)
-      
 }
+
+function goodOrBadWeather(weatherCode) {
+  const goodWeatherCodes = [500, 501, 600, 601, 701, 800, 801, 802];
+  return goodWeatherCodes.includes(weatherCode) ? "good" : "bad";
+}
+
+export default function WeatherBackground(props) {
+  return (`${
+    weatherBackground[dayOrNight(props.sunrise, props.sunset, props.dt)][
+    goodOrBadWeather(props.weather[0].id)]
+  }`)
+}
+
+export { dayOrNight };
