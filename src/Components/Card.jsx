58712d78827e923e15af560dd98a4e21792d6399
Sofia <sofiaq1@gmail.com>
2022-01-26 00:43:28 +0100

    Moved out weatherBackground functions

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -8,24 +8,7 @@ import WeekDay from "./WeekDay";
 import Text from "./Text";
 import List from "./List";
 import ForecastListItem from "./ForecastListItem";
-
-const weatherBackground = {
-  day: { good: "#adcbff, #5c87ff", bad: "#cdd6e5, #7d93cf" },
-  night: { good: "#002f80, #00144d", bad: "#212d40, #16203c" },
-};
-function dayOrNight(sunrise, sunset, currentTime) {
-  return currentTime >= sunrise && currentTime <= sunset
-    ? "day"
-    : currentTime < sunrise
-    ? "night"
-    : currentTime > sunset
-    ? "night"
-    : "day";
-}
-function goodOrBadWeather(weatherCode) {
-  const goodWeatherCodes = [500, 501, 600, 601, 701, 800, 801, 802];
-  return goodWeatherCodes.includes(weatherCode) ? "good" : "bad";
-}
+import WeatherBackground from "./WeatherBackground";
 
 const StyledCard = styled.div`
   height: 100%;
@@ -33,14 +16,8 @@ const StyledCard = styled.div`
   height: fit-content;
   margin: auto;
   z-index: 1;
-  background-image: linear-gradient(
-    ${(props) =>
-      props.data &&
-      weatherBackground[
-        dayOrNight(props.data.sunrise, props.data.sunset, props.data.dt)
-      ][goodOrBadWeather(props.data.weather[0].id)]}
-  );
   box-shadow: 0px 0px 20px #121212;
+  background-image: linear-gradient(${(props) => WeatherBackground(props.data)});
 `;
 
 const CenteredContainer = styled.div`
@@ -54,29 +31,31 @@ const CenteredContainer = styled.div`
 
 export default function Card() {
   const { weatherData } = useContext(weatherContext);
+  const hour = new Date(1643094840 * 1000);
+  console.log(hour)
+  console.log(hour.getHours());
+
 
   const currentDate = new Date(weatherData.current.dt * 1000);
-  console.log(currentDate);
-  console.log(weatherData);
-
+  console.log(currentDate.getHours())
   return (
     <StyledCard data={weatherData.current}>
       <CardThumbnail>
-        <WeatherIcon timeOfDay={dayOrNight(weatherData.current.sunrise, weatherData.current.sunset, weatherData.current.dt)}></WeatherIcon>
+        <WeatherIcon data={weatherData.current}/>
         <CenteredContainer>
           <Text Big className="degree">
             {Math.round(weatherData.current.temp)}
           </Text>
-          <Text size="1.3em">
-            {weatherData.current.weather[0].main}
-          </Text>
+          <Text size="1.3em">{weatherData.current.weather[0].main}</Text>
           <Text size="0.9em">
             <WeekDay />
           </Text>
         </CenteredContainer>
       </CardThumbnail>
       <CardInformation>
-        <List><ForecastListItem/></List>
+        <List>
+          <ForecastListItem />
+        </List>
       </CardInformation>
     </StyledCard>
   );
