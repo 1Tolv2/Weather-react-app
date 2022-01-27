108c842a1b6a69ea4e22273c0c360cc37ef7e06e
Sofia <sofiaq1@gmail.com>
2022-01-27 23:48:23 +0100

    Added timeOfDay variations to sun

--- src/Components/WeatherIcon.jsx
+++ src/Components/WeatherIcon.jsx
@@ -1,5 +1,5 @@
 import React, {useContext} from "react";
-import styled from "styled-components";
+import styled, {css} from "styled-components";
 import { dayOrNight } from "./WeatherBackground";
 import { weatherContext } from "../App";
 
@@ -8,11 +8,8 @@ import { weatherContext } from "../App";
 2. Sun moving as time progresses*/
 const IconContainer = styled.div`
   position: relative;
-  top: 30px;
-  left: 30px;
   width: 40px;
   height: 40px;
-  background-color: ${props => props.timeOfDay === "day" ? "#ffe770" : "#fff3ad"};
   border-radius: 50px;
   z-index: 5;
   &::before {
@@ -22,7 +19,6 @@ const IconContainer = styled.div`
     left: -20px;
     width: 80px;
     height: 80px;
-    background-color: ${props => props.timeOfDay === "day" ? "#ffe770" : "#fff6c2"};
     border-radius: 50px;
     opacity: 0.3;
     z-index: 4;
@@ -34,15 +30,56 @@ const IconContainer = styled.div`
     left: -35px;
     width: 110px;
     height: 110px;
-    background-color: ${props => props.timeOfDay === "day" ? "#ffe770" : "#fff6c2"};
     border-radius: 100px;
     opacity: 0.2;
     z-index: 4;
   }
+
+  ${props => {
+    if (props.timeOfDay === "sunrise") {
+      return css`
+      top: 220px;
+      left: 220px;
+      &::before{
+        background-color: #fff6c2;
+      }
+      &::after{
+        background-color: #fff6c2;
+        width: 120px;
+        height: 120px;
+        border-radius: 120px;
+        top:-40px;
+        left: -40px;
+      }`}
+    else if ( props.timeOfDay === "sunset") {
+      return css`
+      top: 220px;
+      left: 40px;
+      &::before{
+        background-color: #fff6c2;
+      }
+      &::after{
+        background-color: #fff6c2;
+        width: 120px;
+        height: 120px;
+        border-radius: 120px;
+        top:-40px;
+        left: -40px;`
+    } else {
+      return css`
+      top: 50px;
+      left: 50px;
+      background-color: ${props => props.timeOfDay === "day" ? "#ffe770" : "#fff6c2"};
+      &::before{
+        background-color: ${props => props.timeOfDay === "day" ? "#ffe770" : "#fff6c2"};
+      }
+      &::after{
+        background-color: ${props => props.timeOfDay === "day" ? "#ffe770" : "#fff6c2"};`
+    }
+  }}
 `;
 export default function WeatherIcon(props) {
   const { weatherData } = useContext(weatherContext);
 
-console.log()
   return <IconContainer timeOfDay={dayOrNight(props.data.sunrise, props.data.sunset, props.data.dt)}></IconContainer>;
 }
